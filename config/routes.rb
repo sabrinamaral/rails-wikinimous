935aad9322fa73be4123f8183c4b36e081ca1c11
sabrina amaral <sabrinamaraldev@gmail.com>
2023-07-09 11:52:08 -0300

    routes for read an create

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # read all
+  get 'articles', to: 'articles#index'
+  # create
+  get 'articles/new', to: 'articles#new'
+  post 'articles', to: 'articles#create'
+  # read one
+  get 'articles/:id', to: 'articles#show', as: 'article'
+  # update
+  # delete
 end
